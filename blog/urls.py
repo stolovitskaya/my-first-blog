308a0b87745746732e9177b3a3886b20d644ace3
stolovitskaya <stolowitskaya@gmail.com>
2017-05-20 16:33:42 +0300

    html

--- blog/urls.py
+++ blog/urls.py
@@ -1,2 +1,6 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+]
